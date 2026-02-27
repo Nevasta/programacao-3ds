806b1e809288b8d70d5aa29e139b11d64e5fc90c
Nevasta <thyagogafs@gmail.com>
2026-02-27 11:07:56 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# Declara as variáveis numéricas
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Configura a lógica das operações
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+# Exibe os resultados
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
